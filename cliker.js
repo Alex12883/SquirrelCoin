27b0ee6b704ef731f95f7edcb7e72a42cc4fed6b
Alex12883 <106988035+Alex12883@users.noreply.github.com>
2024-06-19 19:04:25 +0300

    Update cliker.js

--- cliker.js
+++ cliker.js
@@ -3,7 +3,7 @@ const voteCount = document.getElementById('vote-count');
 let points = 0;
 let add_points_tap = 2;
 voteImage.addEventListener('click', () => {
-    points++;
-    voteCount.textContent = `${points} балл${points === add_points_tap ? '' : 'а'}`;
+    points = points + add_points_tap;
+    voteCount.textContent = `${points} SQ${points === 1 ? '' : 'а'}`;
     // Добавьте здесь логику сохранения баллов на сервере (например, через AJAX).
 });
