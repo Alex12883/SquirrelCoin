edde8561fb3cee840c092e4784449ca06b27d35e
Alex12883 <106988035+Alex12883@users.noreply.github.com>
2024-06-19 19:01:59 +0300

    Update cliker.js

--- cliker.js
+++ cliker.js
@@ -1,7 +1,7 @@
 const voteImage = document.getElementById('vote-image');
 const voteCount = document.getElementById('vote-count');
 let points = 0;
-let add_points_tap = 1;
+let add_points_tap = 2;
 voteImage.addEventListener('click', () => {
     points++;
     voteCount.textContent = `${points} балл${points === add_points_tap ? '' : 'а'}`;
